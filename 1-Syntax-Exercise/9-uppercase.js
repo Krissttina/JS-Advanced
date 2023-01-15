ad21bd30d76c22cd57d4ed7459fd66c8756fc96e
Kristina <krismetodieva123@gmail.com>
2023-01-15 13:39:52 +0200

    66/100

---
+++ 1-Syntax-Exercise/9-uppercase.js
@@ -0,0 +1,13 @@
+function solve(input){
+    let pattern = /[A-Za-z]+/g;
+    let first = input.match(pattern);
+    let newArr = [];
+
+    for(let i = 0; i < first.length; i++){
+        let cur = first[i];
+        newArr.push(cur.toUpperCase());
+    }
+    console.log(newArr.join(' '));
+}
+
+solve('Hi, how are you?');
