a24cdd021527090ee2814acc66e48d2704aa2ade
Kristina <krismetodieva123@gmail.com>
2023-01-22 20:59:06 +0200

    100/100

---
+++ 3-Object/3-cityTaxes.js
@@ -0,0 +1,24 @@
+function cityRecord(name, population, treasury){
+    const result = {
+        name: name,
+        population: population,
+        treasury: treasury,
+        taxRate: 10,
+        collectTaxes() {
+           this.treasury += this.taxRate * this.population; 
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor(this.population * percentage / 100);
+        },
+        applyRecession(percentage) {
+            this.treasury -= Math.floor(this.treasury * percentage / 100);
+        }
+    }
+    return result;
+}
+
+cityRecord('Tortuga',
+
+7000,
+
+15000)
