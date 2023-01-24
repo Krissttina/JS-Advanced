a5d02de5e795f9bfa42c0e247cf05b8c67d3c173
Kristina <krismetodieva123@gmail.com>
2023-01-24 21:29:17 +0200

    100/100

---
+++ 4-DOM/2-collectList.js
@@ -0,0 +1,11 @@
+function collectList(){
+    const list = document.getElementById('items');
+    const items = Array.from(list.children);
+
+    const result = items
+        .map(li => li.textContent)
+        .join('\n');
+
+        const output = document.getElementById('result');
+        output.value = result;
+}
