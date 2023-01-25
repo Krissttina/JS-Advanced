92d454c3e8bc383d7a1cd5506ff50bf2b23b64f7
Kristina <krismetodieva123@gmail.com>
2023-01-25 11:02:49 +0200

    100/100

---
+++ 4-DOM/4-readMore.js
@@ -0,0 +1,7 @@
+function readMore(){
+    const text = document.getElementById('text');
+    text.style.display = 'inline';
+
+    const button = document.getElementById('more');
+    button.style.display = 'none';
+}
