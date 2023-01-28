cd95ed9c3dc235a21da6a9b48278337494bba916
Kristina <krismetodieva123@gmail.com>
2023-01-28 18:57:45 +0200

    100/100

---
+++ 6-funcs/5-command.js
@@ -0,0 +1,31 @@
+function solve(){
+    let string = '';
+
+    function append(str){
+        string += str;
+    }
+
+    function print(){
+        console.log(string);
+    }
+
+    function removeStart(n){
+        string = string.slice(n);
+    }
+
+    function removeEnd(n){
+        string = string.slice(0, -n);
+    }
+
+    return {
+        append,
+        removeStart,
+        removeEnd,
+        print
+    }
+}
+
+const myProc = solve();
+
+myProc.append('hello');
+myProc.print();
