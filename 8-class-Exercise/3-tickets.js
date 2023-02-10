5118ef88fb1956b46b7fad64b1a2d65703da9c93
Kristina <krismetodieva123@gmail.com>
2023-02-11 00:45:34 +0200

    100/100

---
+++ 8-class-Exercise/3-tickets.js
@@ -0,0 +1,35 @@
+function solve(arr, criterion){
+    class Ticket{
+ constructor(destination, price, status) {
+        this.destination = destination;
+        this.price = price;
+        this.status = status;
+        }   
+    }
+
+   const tickets = [];
+
+   for (const line of arr) {
+    let [destination, price, status] = line.split('|');
+    price = Number(price);
+    const curTicket = new Ticket(destination, price, status);
+    tickets.push(curTicket);
+   }
+
+   const sorted = tickets.sort((a, b) => {
+    if(criterion === 'destination'){
+        return a.destination.localeCompare(b.destination);
+    }else if(criterion === 'price'){
+        return a.price - b.price;
+    }else if( criterion === 'status'){
+        return a.status.localeCompare(b.status);
+    }
+   });
+return sorted;    
+}
+
+console.table(solve(['Philadelphia|94.20|available',
+'New York City|95.99|available',
+'New York City|95.99|sold',
+'Boston|126.20|departed'],
+'destination'))
